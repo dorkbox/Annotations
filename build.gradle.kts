684af5afc503f4681422663e6aaf13af5e9a81f2
Robinson <robinson@dorkbox.com>
2026-02-04 17:12:42 +0100

    build pipeline update

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,32 +24,41 @@
 gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
 
 plugins {
-    id("com.dorkbox.GradleUtils") version "4.6"
+    id("com.dorkbox.GradleUtils") version "4.8"
     id("com.dorkbox.Licensing") version "3.1"
-    id("com.dorkbox.VersionUpdate") version "3.1"
-    id("com.dorkbox.GradlePublish") version "2.0"
+    id("com.dorkbox.VersionUpdate") version "3.2"
+    id("com.dorkbox.GradlePublish") version "2.2"
 
     kotlin("jvm") version "2.3.0"
-}
-
-object Extras {
-    // set for the project
-    const val description = "Extremely fast, lightweight annotation scanner for the classpath, classloader, or files for Java 25"
-    const val group = "com.dorkbox"
-    const val version = "3.3"
-
-    // set as project.ext
-    const val name = "Annotations"
-    const val id = "Annotations"
-    const val vendor = "Dorkbox LLC"
-    const val vendorUrl = "https://dorkbox.com"
-    const val url = "https://git.dorkbox.com/dorkbox/Annotations"
 }
 
 ///////////////////////////////
 /////  assign 'Extras'
 ///////////////////////////////
-GradleUtils.load("$projectDir/../../gradle.properties", Extras)
+GradleUtils.load {
+    group = "com.dorkbox"
+    id = "Annotations"
+
+    description = "Extremely fast, lightweight annotation scanner for the classpath, classloader, or files for Java 25"
+    name = "Annotations"
+    version = "3.3"
+
+    vendor = "Dorkbox LLC"
+    vendorUrl = "https://dorkbox.com"
+
+    url = "https://git.dorkbox.com/dorkbox/Annotations"
+
+    issueManagement {
+        url = "$url/issues"
+        nickname = "Gitea Issues"
+    }
+
+    developer {
+        id = "dorkbox"
+        name = vendor
+        email = "email@dorkbox.com"
+    }
+}
 GradleUtils.defaults()
 GradleUtils.compileConfiguration(JavaVersion.VERSION_25)
 
@@ -71,43 +80,4 @@ licensing {
 
 dependencies {
     api("com.dorkbox:Updates:1.3")
-}
-
-tasks.jar.get().apply {
-    manifest {
-        // https://docs.oracle.com/javase/tutorial/deployment/jar/packageman.html
-        attributes["Name"] = Extras.name
-
-        attributes["Specification-Title"] = Extras.name
-        attributes["Specification-Version"] = Extras.version
-        attributes["Specification-Vendor"] = Extras.vendor
-
-        attributes["Implementation-Title"] = "${Extras.group}.${Extras.id}"
-        attributes["Implementation-Version"] = GradleUtils.now()
-        attributes["Implementation-Vendor"] = Extras.vendor
-    }
-}
-
-mavenCentral {
-    groupId = Extras.group
-    artifactId = Extras.id
-    version = Extras.version
-
-    name = Extras.name
-    description = Extras.description
-    url = Extras.url
-
-    vendor = Extras.vendor
-    vendorUrl = Extras.vendorUrl
-
-    issueManagement {
-        url = "${Extras.url}/issues"
-        nickname = "Gitea Issues"
-    }
-
-    developer {
-        id = "dorkbox"
-        name = Extras.vendor
-        email = "email@dorkbox.com"
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 dorkbox, llc
+ * Copyright 2026 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,3 +14,6 @@
  * limitations under the License.
  */
 rootProject.name = "Annotations"
+//includeBuild("../../public_projects_build_system/GradleUtils")
+//includeBuild("../../public_projects_build_system/GradlePublish")
+//includeBuild("../../public_projects_build_system/VersionUpdate")
